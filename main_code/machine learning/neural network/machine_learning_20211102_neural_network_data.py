9b71f77b33ba0f42559a1f2cec6420c90070d50c
Kimyihwan <kimleehwan3@gmail.com>
2021-11-03 10:11:28 +0900

    20211103

--- main_code/machine learning/neural network/machine_learning_20211102_neural_network_data.py
+++ main_code/machine learning/neural network/machine_learning_20211102_neural_network_data.py
@@ -8,58 +8,41 @@ df_data = pd.read_pickle('data_process/conclusion/sample/hedonic_full_data.pkl')
 
 # normalization 이 필요한 변수 select
 indep_var = ['per_Pr', 'old', 'old_sq', 'car_per', 'area', 'room', 'toilet', 'floor', 'floor_sq',
-             'FAR', 'BC', 'Efficiency']
+             'FAR', 'BC', 'Efficiency', 'H1', 'H2', 'T1', 'T2', 'C1', 'first', 'log_num']
 
 distance_var = ['dist_high', 'dist_sub', 'dist_park']
 
-df_data_edit = df_data[indep_var + distance_var]
-
-# data normalization
-df_data_physical = np.log(df_data_edit)
-df_data_norm = df_data_physical.copy()
-
-# add normalization variables
-df_data_norm['log_num'] = df_data['log_num']
-df_data_norm['first'] = df_data['first']
-
-df_data_norm['H1'] = df_data['H1']
-df_data_norm['H2'] = df_data['H2']
-df_data_norm['T1'] = df_data['T1']
-df_data_norm['T2'] = df_data['T2']
-df_data_norm['C1'] = df_data['C1']
-
+gu = []
 len_gu = 25  # i
 len_time = 24  # j
 for i in range(len_gu):
     a = 'GU' + str(i+1)
-    df_data_norm[a] = df_data[a]
+    gu.append(a)
 
+time = []
 for i in range(len_time):
     b = 'Half' + str(i+1)
-    df_data_norm[b] = df_data[b]
+    time.append(b)
 
-df_data_norm.to_pickle('data_process/conclusion/NN/normalization_without_interaction.pkl')
+variables = indep_var + distance_var +gu[1:] + time[1:]
+
+df_without = df_data[variables]
+
+df_without.to_pickle('data_process/conclusion/NN/normalization_without_interaction.pkl')
 
 #######################################################################################################################
 # with interaction term
-df_data_norm2 = df_data_physical.copy()
-
-# add normalization variables
-df_data_norm2['log_num'] = df_data['log_num']
-df_data_norm2['first'] = df_data['first']
-
-df_data_norm2['H1'] = df_data['H1']
-df_data_norm2['H2'] = df_data['H2']
-df_data_norm2['T1'] = df_data['T1']
-df_data_norm2['T2'] = df_data['T2']
-df_data_norm2['C1'] = df_data['C1']
-
 len_gu = 25  # i
 len_time = 24  # j
 
+inter = []
 for i in range(len_gu):
     for j in range(len_time):
         c = 'i' + str(i+1) + ',' + str(j+1)
-        df_data_norm2[c] = df_data[c]
+        inter.append(c)
 
-df_data_norm2.to_pickle('data_process/conclusion/NN/normalization_with_interaction.pkl')
+variables2 = indep_var + distance_var + inter[1:]
+
+df_with = df_data[variables2]
+
+df_with.to_pickle('data_process/conclusion/NN/normalization_with_interaction.pkl')
